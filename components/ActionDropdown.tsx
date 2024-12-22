cc1872ac5a90ad5dd260632e61a6f8a9b7384ad6
Somnath Goswami <somnath.goswami@cginfinity.com>
2024-12-23 01:42:49 +0530

    added cards for displaying the files

---
+++ components/ActionDropdown.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+const ActionDropdown = () => {
+  return (
+    <div>ActionDropdown</div>
+  )
+}
+
+export default ActionDropdown
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
 import { InputFile } from "node-appwrite/file";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { createAdminClient } from "../appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -57,5 +58,56 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, "Failed to upload file");
+  }
+};
+
+const createQueries = (
+  currentUser: Models.Document
+  //   types: string[],
+  //   searchText: string,
+  //   sort: string,
+  //   limit?: number
+) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
+
+  //   if (types.length > 0) queries.push(Query.equal("type", types));
+  //   if (searchText) queries.push(Query.contains("name", searchText));
+  //   if (limit) queries.push(Query.limit(limit));
+
+  //   if (sort) {
+  //     const [sortBy, orderBy] = sort.split("-");
+
+  //     queries.push(
+  //       orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+  //     );
+  //   }
+
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error("User not found");
+
+    const queries = await createQueries(currentUser);
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries
+    );
+
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, "Failed to get files");
   }
 };

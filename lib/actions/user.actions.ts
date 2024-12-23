4fca7edb31f8314af53b4b954a2e75d89226ff9f
Somnath Goswami <somnath.goswami@cginfinity.com>
2024-12-23 21:11:03 +0530

    fixed deployment error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -108,7 +108,7 @@ export const getCurrentUser = async () => {
 
     return parseStringify(user.documents[0]);
   } catch (error) {
-    handleError(error, "Failed to get current user");
+    console.log(error);
   }
 };
 
